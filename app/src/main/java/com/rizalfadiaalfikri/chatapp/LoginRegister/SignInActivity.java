271e5f6f6ebe5585d54bb396edfcf9c6401984ce
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2022-10-09 12:04:10 +0700

    Add Bottom Navigation View and Fragment

--- app/src/main/java/com/rizalfadiaalfikri/chatapp/LoginRegister/SignInActivity.java
+++ app/src/main/java/com/rizalfadiaalfikri/chatapp/LoginRegister/SignInActivity.java
@@ -2,15 +2,34 @@ package com.rizalfadiaalfikri.chatapp.LoginRegister;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.blogspot.atifsoftwares.animatoolib.Animatoo;
+import com.rizalfadiaalfikri.chatapp.MainActivity;
 import com.rizalfadiaalfikri.chatapp.R;
 
 public class SignInActivity extends AppCompatActivity {
+
+    Button btn_signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        btn_signIn = findViewById(R.id.btn_signIn);
+
+        btn_signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                Animatoo.animateSlideUp(SignInActivity.this);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/rizalfadiaalfikri/chatapp/MainActivity.java
+++ app/src/main/java/com/rizalfadiaalfikri/chatapp/MainActivity.java
@@ -1,14 +1,68 @@
 package com.rizalfadiaalfikri.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.rizalfadiaalfikri.chatapp.Fragments.ChatsFragment;
+import com.rizalfadiaalfikri.chatapp.Fragments.ContatcsFragment;
+import com.rizalfadiaalfikri.chatapp.Fragments.GroupsFragment;
+import com.rizalfadiaalfikri.chatapp.Fragments.RequestsFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        frameLayout = findViewById(R.id.frameLayout);
+
+        replaceFragment(new ChatsFragment());
+
+        BottomNavigation();
+
+    }
+
+    private void BottomNavigation() {
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_chat:
+                        replaceFragment(new ChatsFragment());
+                        break;
+                    case R.id.menu_group:
+                        replaceFragment(new GroupsFragment());
+                        break;
+                    case R.id.menu_contact:
+                        replaceFragment(new ContatcsFragment());
+                        break;
+                    case R.id.menu_request:
+                        replaceFragment(new RequestsFragment());
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
