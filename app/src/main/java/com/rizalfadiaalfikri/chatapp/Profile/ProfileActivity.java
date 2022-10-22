f18a39d28f7a8b2aa458b0ef8f57586e8d45546f
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2022-10-22 18:35:29 +0700

    add retrieveUserInfo, manageChatRequest, sendChatRequest, removeChatRequest

--- app/src/main/java/com/rizalfadiaalfikri/chatapp/Profile/ProfileActivity.java
+++ app/src/main/java/com/rizalfadiaalfikri/chatapp/Profile/ProfileActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -25,7 +28,7 @@ public class ProfileActivity extends AppCompatActivity {
     private Button btn_profile_sendMessage, btn_profile_cancelChatRequest;
 
     private FirebaseAuth mAuth;
-    private DatabaseReference user_ref;
+    private DatabaseReference user_ref, chatRequestRef, ContactsRef;
 
     private String user_id, current_state, current_user_id;
 
@@ -36,9 +39,12 @@ public class ProfileActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         user_ref = FirebaseDatabase.getInstance().getReference().child("Users");
+        chatRequestRef = FirebaseDatabase.getInstance().getReference().child("Request");
+
 
         user_id = getIntent().getStringExtra("user_id");
         current_user_id = mAuth.getCurrentUser().getUid();
+        current_state = "new";
 
         initializefields();
         retrieveUserInfo();
@@ -66,12 +72,127 @@ public class ProfileActivity extends AppCompatActivity {
                             txt_profile_name.setText(username);
                             txt_profile_status.setText(status);
 
+                            manageChatRequest();
+
                         }
                     }
 
                     @Override
                     public void onCancelled(@NonNull DatabaseError error) {
 
+                    }
+                });
+    }
+
+    private void manageChatRequest() {
+        chatRequestRef.child(current_user_id)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.hasChild(user_id)) {
+                            String request_type = snapshot.child(user_id).child("request_type").getValue().toString();
+                            if (request_type.equals("sent")) {
+                                current_state = "request_sent";
+                                btn_profile_sendMessage.setText("Cancel Chat Request");
+                            } else if (request_type.equals("received")) {
+                                current_state = "request_received";
+                                btn_profile_sendMessage.setText("Accept Chat Request");
+
+                                btn_profile_cancelChatRequest.setVisibility(View.VISIBLE);
+                                btn_profile_cancelChatRequest.setEnabled(true);
+                                btn_profile_cancelChatRequest.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        removeChatRequest();
+                                    }
+                                });
+
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+        if (!current_state.equals(user_id)) {
+            btn_profile_sendMessage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    btn_profile_sendMessage.setEnabled(false);
+
+                    if (current_state.equals("new")) {
+                       sendChatRequest();
+                    }
+                    if (current_state.equals("request_sent")) {
+                        removeChatRequest();
+                    }
+                    if (current_state.equals("request_received")) {
+                        acceptRequest();
+                    }
+                }
+            });
+        }
+    }
+
+    private void acceptRequest() {
+
+    }
+
+    private void removeChatRequest() {
+        chatRequestRef.child(current_user_id)
+                .child(user_id)
+                .removeValue()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            chatRequestRef.child(user_id)
+                                    .child(current_user_id)
+                                    .removeValue()
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                btn_profile_sendMessage.setEnabled(true);
+                                                current_state = "new";
+                                                btn_profile_sendMessage.setText("Send Message");
+
+//                                                btn_profile_cancelChatRequest.setVisibility(View.VISIBLE);
+//                                                btn_profile_cancelChatRequest.setEnabled(false);
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                });
+    }
+
+    private void sendChatRequest() {
+        chatRequestRef.child(current_user_id).child(user_id)
+                .child("request_type")
+                .setValue("sent")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            chatRequestRef.child(user_id).child(current_user_id)
+                                    .child("request_type")
+                                    .setValue("received")
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                btn_profile_sendMessage.setEnabled(true);
+                                                current_state = "request_sent";
+                                                btn_profile_sendMessage.setVisibility(View.INVISIBLE);
+
+                                                btn_profile_cancelChatRequest.setVisibility(View.VISIBLE);
+                                            }
+                                        }
+                                    });
+                        }
                     }
                 });
     }
